0e16ab3450567e50e4499dc069c68ef898b5e023
Onel Hirmez <47871518+onel95@users.noreply.github.com>
2020-12-07 17:41:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 // Starts the program logic when the button is clicked.
-function controlClick() {
-    document.getElementById("showInfo").onclick = () => {
+function runClick() {
+    var httpRequest;
+    document.getElementById("showInfo").onclick = 
     sendRequest("myDegrees.json");
-    };
-}
+
 
 // Checks validity of request and its status and sends it.
 function sendRequest(url) {
-    var httpRequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest();
     // Check if instance was created.
     if (!httpRequest) {
         console.log("Failed to create an XMLHttp instance.");
         return false;
     }
 
+    // Call the arrow function when the state of the request changes.
+    httpRequest.onreadystatechange = showContents;
+
     // Make the request as GET and to the json file. 
     httpRequest.open("GET", url);
     // Send the request.
     httpRequest.send();
-
-    // Call the arrow function when the state of the request changes.
-    httpRequest.onreadystatechange = () => {
+   }
+    
+    function showContents() {
         // Check if request is ready.
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            console.log("entered the first if.");
             // Check server response code. 200 means a successful call.
             if (httpRequest.status === 200) {
                 document.getElementById("ajax-info").innerHTML = httpRequest.responseText;
@@ -30,9 +34,6 @@ function sendRequest(url) {
             else {
                 console.log("Request status was not 200.");
             }
-        }
-        else {
-            console.log("There was a problem with the request ready status.");
-        }
-    };
-}
+        }      
+    };   
+};
